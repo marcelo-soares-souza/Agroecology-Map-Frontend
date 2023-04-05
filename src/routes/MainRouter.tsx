518d71d92b3288135b9ecf0cc47d73fe16db59d1
rbwestphalen <rbwestphalen@gmail.com>
2023-04-05 19:50:00 -0300

    feat: register page route

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -4,19 +4,24 @@ import ErrorPage from "../pages/ErrorPage";
 import RootLayout from "./RouteLayout";
 
 import Home from "../pages/Home";
+import { RegisterForm } from "../pages/RegisterForm";
 
 import "../index.css";
 
 export const MainRouter = createBrowserRouter([
-  {
-    path: "/",
-    errorElement: <ErrorPage />,
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		errorElement: <ErrorPage />,
+		element: <RootLayout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/register",
+				element: <RegisterForm />,
+			},
+		],
+	},
 ]);
