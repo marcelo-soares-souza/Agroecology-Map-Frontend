ec2d237c49568912ae858aafda52799fc7980f3a
rbwestphalen <rbwestphalen@gmail.com>
2023-04-21 01:14:48 -0300

    style-fix: register page spacing fix

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from "react-i18next";
 import Translator from "../components/i18n/Translator";
 
+import classes from "./Register.module.css";
+
 const Register = () => {
   return (
     <div className="grid-container">
@@ -8,43 +10,46 @@ const Register = () => {
         <div className="contain-center">
           <p>IMAGEM</p>
         </div>
-        <div>
-          <h1>
-            <Translator path="register.heading" />
-          </h1>
-          <p>
-            <Translator path="register.description" />
-          </p>
-          <form method="POST" action="/register">
-            <label htmlFor="email" hidden>
-              E-mail
-            </label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              placeholder="E-mail"
-              required
-            />
-            <label htmlFor="password" hidden>
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              name="password"
-              placeholder={
-                useTranslation().t("register.passwordPlaceholder") ?? "Password"
-              }
-              required
-            />
-            <a href="/login">
-              <Translator path="register.linkToLogin" />
-            </a>
-            <button type="submit">
-              <Translator path="form.submit" />
-            </button>
-          </form>
+        <div className="contain-center">
+          <div>
+            <h1>
+              <Translator path="register.heading" />
+            </h1>
+            <p className="light-text">
+              <Translator path="register.description" />
+            </p>
+            <form className={classes.form} method="POST" action="/register">
+              <label htmlFor="email" hidden>
+                E-mail
+              </label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                required
+              />
+              <label htmlFor="password" hidden>
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder={
+                  useTranslation().t("register.passwordPlaceholder") ??
+                  "Password"
+                }
+                required
+              />
+              <a href="/login">
+                <Translator path="register.linkToLogin" />
+              </a>
+              <button type="submit">
+                <Translator path="form.submit" />
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
