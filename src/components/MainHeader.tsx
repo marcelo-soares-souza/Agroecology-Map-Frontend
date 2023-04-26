654da84143f61358a293108eda510dcc4b385c03
rbwestphalen <rbwestphalen@gmail.com>
2023-04-25 21:56:34 -0300

    style: logotype updated

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -2,19 +2,14 @@ import { NavLink } from "react-router-dom";
 
 import classes from "./MainHeader.module.css";
 
-import logo from "/logo.svg";
+import logo from "../assets/logo-agroecologymap.svg";
 import Translator from "./i18n/Translator";
 
 const MainHeader = () => {
   return (
     <header className={classes.header}>
       <NavLink className={classes.logoWrap} to="/">
-        <img
-          height={100}
-          src={logo}
-          className={classes.logo}
-          alt="Agroecology Map Logo"
-        />
+        <img src={logo} className={classes.logo} alt="Agroecology Map Logo" />
         <span className={classes.logotype}>
           <Translator path="header.agroecologyMap" />
         </span>
