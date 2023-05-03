44b04ad19c521143288999b564148d53c7689924
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-05-03 18:17:27 -0300

    feat: country and state select with react-select and country-state-city

--- src/components/Profile/WhereAreYouFrom.tsx
+++ src/components/Profile/WhereAreYouFrom.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Country, State, City, ICountry, IState } from "country-state-city";
+import Select, { ActionMeta } from "react-select";
 
 import classes from "../../pages/Profile.module.css";
 
@@ -17,28 +19,58 @@ const WhereAreYouFrom = (props: IWhereAreYouFrom) => {
   const [state, setState] = useState("");
   const [city, setCity] = useState("");
 
+  const [cscCountry, setCscCountry] = useState<ICountry>({} as ICountry);
+  const [cscState, setCscState] = useState<IState>({} as IState);
+
   useEffect(() => {
     setCountry(props.country);
     setState(props.state);
     setCity(props.city);
   }, []);
 
+  const updatedCountries = Country.getAllCountries().map(
+    (uCountry: ICountry) => ({
+      label: uCountry.name,
+      value: uCountry.isoCode,
+      ...uCountry
+    })
+  );
+
+  const updatedStates = (isoCode: string) =>
+    State.getStatesOfCountry(isoCode).map((uState) => ({
+      label: uState.name,
+      value: uState.isoCode,
+      ...uState
+    }));
+
   const previousPageHandler = () => {
     props.changePage("AboutYou");
   };
 
   const countryChangedHandler = (
-    event: React.ChangeEvent<HTMLInputElement>
+    country: ICountry | null,
+    actionMeta: ActionMeta<ICountry>
   ) => {
-    const value = event.target.value;
-    setCountry(value);
-    props.onCountryChange(value);
+    if (country) {
+      setCscCountry(country);
+      setCountry(country.name);
+      props.onCountryChange(country.name);
+
+      setCscState({} as IState);
+      setState("");
+      props.onStateChange("");
+    }
   };
 
-  const stateChangedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setState(value);
-    props.onStateChange(value);
+  const stateChangedHandler = (
+    state: IState | null,
+    actionMeta: ActionMeta<IState>
+  ) => {
+    if (state) {
+      setCscState(state);
+      setState(state.name);
+      props.onStateChange(state.name);
+    }
   };
 
   const cityChangedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -55,21 +87,27 @@ const WhereAreYouFrom = (props: IWhereAreYouFrom) => {
         </div>
         <div>
           <h3>Country</h3>
-          <input
-            type="text"
-            placeholder="e.g.: Kenya"
+          <Select
+            id="country"
+            name="country"
+            options={updatedCountries}
             onChange={countryChangedHandler}
-            value={country}
+            value={cscCountry}
+            className={classes.react_select}
           />
         </div>
 
         <div>
           <h3>State</h3>
-          <input
-            type="text"
-            placeholder="e.g.: Bahia"
+          <Select
+            id="state"
+            name="state"
             onChange={stateChangedHandler}
-            value={state}
+            value={cscState}
+            options={updatedStates(
+              cscCountry.isoCode ? cscCountry.isoCode : ""
+            )}
+            className={classes.react_select}
           />
         </div>
 
